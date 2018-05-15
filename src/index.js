214e1fd14d5f3fd0d44812cdad7bfe77cf066bc4
Rares Stelian <raresstelian@gmail.com>
2018-05-15 23:15:03 +0300

    update package.json with youtube-api-searc package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React  from "react";
 import ReactDOM from "react-dom";
+
+const API_KEY = "AIzaSyAeiGy_96oUXAg-Qvk0JWVb8jM6fIQJ5Hs";
 // crete a new component and this component should produce some HTML
 
 const App = () => {
