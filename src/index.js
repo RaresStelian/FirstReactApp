845a740ef84970d39efefc251322af34f7026554
Rares Stelian <raresstelian@gmail.com>
2018-05-15 21:37:21 +0300

    first component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import { React } from "react";
-
-import { ReactDOM } from "react-dom";
+import React  from "react";
+import ReactDOM from "react-dom";
 // crete a new component and this component should produce some HTML
 
-const App = function() {
-  return <div>Hi there</div>
-};
+const App = () => {
+  return <div>Hi there</div>;
+}
 
 // take this component generated HTML and put int on the page (in the DOM)
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
 
