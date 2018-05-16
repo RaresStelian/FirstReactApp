b36316c77d6185977014481e9bdc1f468b444ecc
Rares Stelian <raresstelian@gmail.com>
2018-05-17 00:08:59 +0300

    change SearchBar function component in a class component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React  from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/searchBar.js";
 
 const API_KEY = "AIzaSyAeiGy_96oUXAg-Qvk0JWVb8jM6fIQJ5Hs";
-// crete a new component and this component should produce some HTML
 
+// crete a new component and this component should produce some HTML
 const App = () => {
-  return <div>Hi there</div>;
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 // take this component generated HTML and put int on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
-
+ReactDOM.render(<App />, document.querySelector(".container"));
